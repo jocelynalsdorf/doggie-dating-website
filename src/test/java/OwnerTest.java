12092857c6e2f53b23572314dad5b68b1964c4a8
Morgan Lutz <morgaface@macbook1.local>
2015-09-09 10:14:53 -0700

    add initial files

---
+++ src/test/java/OwnerTest.java
@@ -0,0 +1,14 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+import org.junit.Rule;
+import java.util.List;
+import java.util.Arrays;
+import java.util.ArrayList;
+
+public class OwnerTest {
+
+  @Rule
+  public DatabaseRule database = new DatabaseRule();
+
+
+}
